5dd51a73496f08eda610cfc1040cbfa2a4231e65
Omer Basar <omerbasar_97@hotmail.com>
2019-09-05 12:49:56 +0300

    Getting the name of opened window

--- main.go
+++ main.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"fmt"
+	"syscall"
+	"unsafe"
 
 	"golang.org/x/sys/windows"
 )
 
-var (
-	win = windows.NewLazyDLL("user32.dll")
+var win = windows.NewLazyDLL("user32.dll")
+var getWindowText = win.NewProc("GetWindowTextW")
+var getWindowTextLenght = win.NewProc("GetWindowTextLengthW")
+
+type (
+	HANDLE uintptr
+	HWND   HANDLE
 )
 
 func main() {
 	for {
 		hwnd := tW("GetForegroundWindow")
-		fmt.Println(hwnd)
+		getText := getText(HWND(hwnd))
+		fmt.Printf("Now you're in->>  %s // hwnd-> %v \n", getText, hwnd)
 	}
 }
 
@@ -21,4 +29,12 @@ func tW(str string) uintptr {
 	pr := win.NewProc(str)
 	hwnd, _, _ := pr.Call()
 	return hwnd
+}
+
+func getText(hwnd HWND) string {
+	len, _, _ := getWindowTextLenght.Call(uintptr(hwnd))
+	r := int(len) + 1 //have to +1
+	buf := make([]uint16, r)
+	getWindowText.Call(uintptr(hwnd), uintptr(unsafe.Pointer(&buf[0])), uintptr(r))
+	return syscall.UTF16ToString(buf)
 }
